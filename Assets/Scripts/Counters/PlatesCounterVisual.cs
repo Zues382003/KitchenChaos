3ea11c82157946fc0189a728a4182d3dac017e5d
Zues383003 <nguyenminhchanh3842@gmail.com>
2025-12-09 11:08:37 +0700

    Complete PlateCounter and PlateIconUI

---
+++ Assets/Scripts/Counters/PlatesCounterVisual.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounterVisual : MonoBehaviour
+{
+    [SerializeField] private PlatesCounter platesCounter;
+    [SerializeField] private Transform CounterTopPoint;
+    [SerializeField] private Transform PlateVisualPrefab;
+
+    private List<GameObject> plateVisualGameObjectList;
+
+    private void Awake()
+    {
+        plateVisualGameObjectList = new List<GameObject>();
+    }
+    
+    private void Start()
+    {
+        platesCounter.OnPlateSpawned += PlatesCounter_OnPlateSpawned;
+        platesCounter.OnPlateRemoved += PlatesCounter_OnPlateRemoved;
+        
+    }
+    
+    void PlatesCounter_OnPlateRemoved(object sender, EventArgs e)
+    {
+        GameObject plateGameObject = plateVisualGameObjectList[plateVisualGameObjectList.Count - 1];
+        plateVisualGameObjectList.Remove(plateGameObject);
+        Destroy(plateGameObject);
+    }
+    
+    void PlatesCounter_OnPlateSpawned(object sender, EventArgs e)
+    {
+        Transform plateVisualTransform = Instantiate(PlateVisualPrefab, CounterTopPoint);
+        float plateOffsetY = 0.1f;
+        plateVisualTransform.localPosition = new Vector3(0f, plateOffsetY * plateVisualGameObjectList.Count, 0f);
+        
+        plateVisualGameObjectList.Add(plateVisualTransform.gameObject);
+    }
+}
