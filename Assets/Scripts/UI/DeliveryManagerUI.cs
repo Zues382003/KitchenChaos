b54c6abf6c29a5d527dfd0e322faa217d2ef1d9f
Zues383003 <nguyenminhchanh3842@gmail.com>
2025-12-10 10:34:44 +0700

    Completed DeliveryManager and DeliveryManagerIcon

---
+++ Assets/Scripts/UI/DeliveryManagerUI.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+public class DeliveryManagerUI : MonoBehaviour
+{
+    [SerializeField] private Transform container;
+    [SerializeField] private Transform recipeTemplate;
+
+    private void Awake()
+    {
+        recipeTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSpawned += Instance_OnRecipeSpawned;
+        DeliveryManager.Instance.OnRecipeCompleted += Instance_OnRecipeCompleted;
+   
+        UpdateVisual();
+    }
+    
+    void Instance_OnRecipeCompleted(object sender, EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    void Instance_OnRecipeSpawned(object sender, EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        foreach (Transform child in container)
+        {
+            if (child == recipeTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach (RecipeSO recipeSo in DeliveryManager.Instance.GetWaitingRecipeSOList())
+        {
+            Transform recipeTransform = Instantiate(recipeTemplate, container);
+            recipeTransform.gameObject.SetActive(true);
+            recipeTransform.GetComponent<DeliveryManagerSingleUI>().SetRecipeSO(recipeSo);
+        }
+    }
+}
